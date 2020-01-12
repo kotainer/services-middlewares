f5cca6ec621ada6d47f80526960568a7f9810924
Butkov Viktor Nikolaevich <bvn0293@gmail.com>
2020-01-12 22:28:59 +0500

    change target to ES2019

--- src/middlewares/token-auth.middleware.ts
+++ src/middlewares/token-auth.middleware.ts
@@ -1,23 +1,19 @@
 import { AUTH_TOKEN_KEY } from '../consts/system';
 
-const send401 = () => {
-    throw {
-        status: 401,
-        message: 'Пользователь неавторизирован',
-    }
-}
-
 export default (cacheService: any) => {
     return async (ctx: any, next: ()=> {}) => {
         const headerToken = ctx.headers.authorization;
-        const user = await cacheService.get(`${AUTH_TOKEN_KEY}_${headerToken}`);
+        const user = cacheService.get(`${AUTH_TOKEN_KEY}_${headerToken}`);
 
         if (!headerToken || !user) {
-            send401();
+            throw {
+                status: 401,
+                message: 'Пользователь неавторизирован',
+            }
         }
-
+    
         ctx.user = user;
-
+    
         next();
     }
 };
