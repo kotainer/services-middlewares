cd302269a4985c1fbb8d91d215a1e13d40b19139
Butkov Viktor Nikolaevich <bvn0293@gmail.com>
2020-01-15 00:10:39 +0500

    fix get user by token

--- src/middlewares/token-auth.middleware.ts
+++ src/middlewares/token-auth.middleware.ts
@@ -3,7 +3,7 @@ import { AUTH_TOKEN_KEY } from '../consts/system';
 export default (cacheService: any) => {
     return async (ctx: any, next: ()=> {}) => {
         const headerToken = ctx.headers.authorization;
-        const user = cacheService.get(`${AUTH_TOKEN_KEY}_${headerToken}`);
+        const user = await cacheService.get(`${AUTH_TOKEN_KEY}_${headerToken}`);
 
         if (!headerToken || !user) {
             throw {
