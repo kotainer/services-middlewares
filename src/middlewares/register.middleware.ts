f37185c3e3099167acb5f7a4d7ac135799d61c76
Butkov Viktor Nikolaevich <bvn0293@gmail.com>
2020-01-18 15:18:50 +0500

    fix register

--- src/middlewares/register.middleware.ts
+++ src/middlewares/register.middleware.ts
@@ -2,9 +2,11 @@
  * Регистрация пользователя
  */
 export default (usersServiceAPI: any) => {
-    return async (ctx: any) => {
+    return async (ctx: any, next: () => {}) => {
         const user = await usersServiceAPI.register(ctx.request.body);
             
         ctx.user = user;
+
+        await next();
     }
 }
